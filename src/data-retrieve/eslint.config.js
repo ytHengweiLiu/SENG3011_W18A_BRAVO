b47311746ba89021a63be96f0a55cc4d4790e86f
Fai Leui Chan <faileui@gmail.com>
2025-03-19 00:40:57 +1100

    migrate eslint file

---
+++ src/data-retrieve/eslint.config.js
@@ -0,0 +1,13 @@
+export default [
+  {
+    env: {
+      node: true,
+      es2021: true,
+      jest: true
+    },
+    rules: {
+      'no-unused-vars': 'warn',
+      'no-console': 'off'
+    }
+  }
+]
