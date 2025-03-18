a446bfca0feedd0066796dcdfcce8c79aabffb3b
Fai Leui Chan <faileui@gmail.com>
2025-03-18 23:37:11 +1100

    rm prod specific suffix

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -8,22 +8,22 @@ echo "Deploying to production workspace: $WORKSPACE"
 # Package data-collect.js
 cd src/data-collect
 npm install
-zip -r ../../terraform/lambda-deployment-package-collect-${WORKSPACE}.zip .
+zip -r ../../terraform/lambda-deployment-package-collect.zip .
 
 # Package data-retrieve.js
 cd ../data-retrieve
 npm install
-zip -r ../../terraform/lambda-deployment-package-retrieve-${WORKSPACE}.zip .
+zip -r ../../terraform/lambda-deployment-package-retrieve.zip .
 
 # Package analytical model
 cd ../analytical-model
 npm install
-zip -r ../../terraform/lambda-deployment-package-analyse-${WORKSPACE}.zip .
+zip -r ../../terraform/lambda-deployment-package-analyse.zip .
 
 # Terraform deployment
 cd ../../terraform
 terraform init
-terraform workspace select ${WORKSPACE} || terraform workspace new ${WORKSPACE}
+terraform workspace select default
 
 echo "Running Terraform Plan for production..."
 terraform plan -out=tfplan
@@ -32,6 +32,6 @@ echo "Applying Terraform changes in production..."
 terraform apply tfplan
 
 # Clean up
-rm lambda-deployment-package-collect-${WORKSPACE}.zip lambda-deployment-package-retrieve-${WORKSPACE}.zip lambda-deployment-package-analyse-${WORKSPACE}.zip
+rm lambda-deployment-package-collect.zip lambda-deployment-package-retrieve.zip lambda-deployment-package-analyse.zip
 
 echo "Production deployment complete!"
