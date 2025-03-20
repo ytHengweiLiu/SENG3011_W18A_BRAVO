b10e3c9a25b06577c89b74f66b6693f2a65f4c56
Fai Leui Chan <faileui@gmail.com>
2025-03-21 00:40:37 +1100

    CORS

--- terraform/main.tf
+++ terraform/main.tf
@@ -379,49 +379,6 @@ resource "aws_api_gateway_integration_response" "retrieve_options_integration_re
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
   }
 }
-
-# Analyse method response (for CORS)
-resource "aws_api_gateway_method_response" "analyse_method_response" {
-  rest_api_id = aws_api_gateway_rest_api.nba_prediction_api.id
-  resource_id = aws_api_gateway_resource.analyse.id
-  http_method = aws_api_gateway_method.analyse_method.http_method
-  status_code = "200"
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin"  = true
-    "method.response.header.Access-Control-Allow-Headers" = true
-    "method.response.header.Access-Control-Allow-Methods" = true
-  }
-
-  response_models = {
-    "application/json" = "Empty"
-  }
-}
-
-# Integration response
-resource "aws_api_gateway_integration_response" "analyse_integration_response" {
-  rest_api_id = aws_api_gateway_rest_api.nba_prediction_api.id
-  resource_id = aws_api_gateway_resource.analyse.id
-  http_method = aws_api_gateway_method.analyse_method.http_method
-  status_code = aws_api_gateway_method_response.analyse_method_response.status_code
-
-  response_parameters = {
-    "method.response.header.Access-Control-Allow-Origin"  = "'*'"
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
-    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
-  }
-
-  # This is needed to match the AWS_PROXY integration
-  response_templates = {
-    "application/json" = ""
-  }
-
-  depends_on = [
-    aws_api_gateway_integration.analyse_integration,
-    aws_api_gateway_method_response.analyse_method_response
-  ]
-}
-
 # Add OPTIONS method for CORS preflight requests
 resource "aws_api_gateway_method" "analyse_options_method" {
   rest_api_id   = aws_api_gateway_rest_api.nba_prediction_api.id
@@ -443,31 +400,45 @@ resource "aws_api_gateway_integration" "analyse_options_integration" {
 }
 
 # Method response for OPTIONS
-resource "aws_api_gateway_method_response" "analyse_options_200" {
+resource "aws_api_gateway_method_response" "analyse_method_response" {
   rest_api_id = aws_api_gateway_rest_api.nba_prediction_api.id
   resource_id = aws_api_gateway_resource.analyse.id
   http_method = aws_api_gateway_method.analyse_options_method.http_method
   status_code = "200"
 
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = true,
-    "method.response.header.Access-Control-Allow-Methods" = true,
     "method.response.header.Access-Control-Allow-Origin"  = true
+    "method.response.header.Access-Control-Allow-Headers" = true
+    "method.response.header.Access-Control-Allow-Methods" = true
+  }
+
+  response_models = {
+    "application/json" = "Empty"
   }
 }
 
-# Integration response for OPTIONS
+# Integration response
 resource "aws_api_gateway_integration_response" "analyse_options_integration_response" {
   rest_api_id = aws_api_gateway_rest_api.nba_prediction_api.id
   resource_id = aws_api_gateway_resource.analyse.id
   http_method = aws_api_gateway_method.analyse_options_method.http_method
-  status_code = aws_api_gateway_method_response.analyse_options_200.status_code
+  status_code = aws_api_gateway_method_response.analyse_method_response.status_code
 
   response_parameters = {
-    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'",
-    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'",
     "method.response.header.Access-Control-Allow-Origin"  = "'*'"
+    "method.response.header.Access-Control-Allow-Headers" = "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'"
+    "method.response.header.Access-Control-Allow-Methods" = "'POST,OPTIONS'"
   }
+
+  # This is needed to match the AWS_PROXY integration
+  response_templates = {
+    "application/json" = ""
+  }
+
+  depends_on = [
+    aws_api_gateway_integration.analyse_integration,
+    aws_api_gateway_method_response.analyse_method_response
+  ]
 }
 
 # API Gateway Deployment
@@ -477,7 +448,6 @@ resource "aws_api_gateway_deployment" "nba_prediction_deployment" {
     aws_api_gateway_integration.scrape_integration,
     aws_api_gateway_integration.retrieve_integration,
     aws_api_gateway_integration.analyse_integration,
-    aws_api_gateway_integration_response.analyse_integration_response,
     aws_api_gateway_integration_response.scrape_options_integration_response,
     aws_api_gateway_integration_response.retrieve_options_integration_response,
     aws_api_gateway_integration_response.analyse_options_integration_response
